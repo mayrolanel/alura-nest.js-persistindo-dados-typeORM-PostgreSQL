5e97be701dacea9865b2166923f857d59edce693
mayro.belizario <mayro.belizario@trinusco.com.br>
2024-11-16 20:08:56 -0300

    feat: :sparkles: add: aula 01 typeOrm

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,15 @@ import { ProdutoModule } from './produto/produto.module';
 import { UsuarioModule } from './usuario/usuario.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostgresConfigService } from './config/db.config';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     UsuarioModule,
     ProdutoModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRootAsync({
       useClass: PostgresConfigService,
       inject: [PostgresConfigService],
